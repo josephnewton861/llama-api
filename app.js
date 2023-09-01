e64967c61a1fab246bb27c085648b621ea7549b4
Joseph Newton <josephnewton@192.168.1.109>
2023-09-01 08:33:03 +0100

    Added home page route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || 8080
 const cors = require('cors');
 app.use(cors)
 const apiRouter = require("./routes/api");

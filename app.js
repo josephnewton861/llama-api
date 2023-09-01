4862997d5f3d8a6fddd0ff34c61fa8e0bf1e4f2c
Joseph Newton <josephnewton@192.168.1.109>
2023-09-01 08:22:41 +0100

    Added home page route

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const cors = require('cors');
 app.use(cors)
 const apiRouter = require("./routes/api");
 
-app.all("/*", (req, res, next) => {
-    res.status(404).send({ msg: "Path not found" });
-  });
-
 app.use(express.json());
+
+app.get('/', (req, res) => res.send('Testing'))
 
 app.use("/api", apiRouter);
 

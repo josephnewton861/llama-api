03dd7b819f670d07ed3dbb676c880a0066dd76db
Joseph Newton <josephnewton@192.168.1.109>
2023-09-01 08:10:45 +0100

    removed next

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const apiRouter = require("express").Router();
 const llamaRouter = require('../routes/llama')
 
-apiRouter.get("/", (req, res, next) => res.send('Testing my API'));
+apiRouter.get("/", (req, res) => res.send('Testing my API'));
 
 apiRouter.use('/llama', llamaRouter)
 
